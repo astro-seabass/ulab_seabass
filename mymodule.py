0c23f98fde34dea7f76eef5f8a3bc09e2d237c2b
Seabass Branch <esby@mac.lan>
2025-02-03 22:51:02 -0800

    upload homework 11

---
+++ mymodule.py
@@ -0,0 +1,6 @@
+def orbital_period_module(a):
+    # calculates the orbital period with Kepler's 3rd Law
+    print("hello there, i am not from the notebook")
+    print("i'm defined in `mymodule.py` :DD")
+    period = (a ** 3) ** 0.5
+    return period
